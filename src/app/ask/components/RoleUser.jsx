523710189f407e9147b80764981a9a3610809511
zenggpzqbx <zenggpzqbx@163.com>
2025-10-25 22:42:27 +0800

    feat:用户输入信息新增复制功能、默认显示工具条

--- src/app/ask/components/RoleUser.jsx
+++ src/app/ask/components/RoleUser.jsx
@@ -1,22 +1,16 @@
 import {RedoOutlined} from "@ant-design/icons";
-import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import Image from "next/image";
 import {useStreamData} from "@/utils/useStreamData";
 import {deleteByIndex} from "@/store/chatMessageSlice";
 import ask from "../ask.module.css"
+import {useState} from "react";
+
 export function RoleUser({content}) {
-    const [showTools, setShowTools] = useState(false);
     const chatMessage = useSelector((state) => state.chatMessage);
     const {handleStreamData} = useStreamData()
     const dispatch = useDispatch();
-
-    function handleMouseOver() {
-        setShowTools(true);
-    }
-
-    function handleMouseLeave() {
-        setShowTools(false);
-    }
+    const [isCopy, setIsCopy] = useState(false);
 
     function handleRedoEvent() {
         const index = chatMessage.length - 1;
@@ -26,19 +20,32 @@ export function RoleUser({content}) {
                 handleStreamData(chatMessage)
             } else {
                 dispatch(deleteByIndex(index))
-                const data = chatMessage.slice(0, index - 1);
+                const data = chatMessage.slice(0, index);
                 handleStreamData(data)
             }
         }
-        setShowTools(false);
+    }
+
+    function handleCopeEvent() {
+        navigator.clipboard.writeText(content).then(result => {
+            setIsCopy(true);
+        })
+        setTimeout(() => {
+            setIsCopy(false);
+        }, 500)
     }
 
     return (
-            <div className={`${ask.messageDefault} ${ask.roleUser} ${ask.messageClient}`} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave} >
-                <div className={`${ask.userTools}`}>
-                    {showTools ? <RedoOutlined onClick={handleRedoEvent}/> : ""}
-                </div>
-                <div>{content}</div>
+        <div className={`${ask.messageDefault}`}>
+            <div className={`${ask.messageClient}`}>{content}</div>
+            <div className={`${ask.userTools}`}>
+                <RedoOutlined onClick={handleRedoEvent}/>
+                {!isCopy ?
+                    (<Image src={"/svg/复制.svg"} alt={"复制"} width={20} height={20}
+                            onClick={handleCopeEvent}></Image>)
+                    : (<Image src={"/svg/对号.svg"} alt={"复制"} width={20} height={20}
+                              onClick={handleCopeEvent}></Image>)}
             </div>
+        </div>
     )
 }
